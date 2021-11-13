c3ffeced3c27aabd48553b4694004256d21b78dd
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-14 02:23:34 +0530

    Update counting.py

--- counting.py
+++ counting.py
@@ -1,9 +1,16 @@
 array = [1,2,3,4,5,2,5,1,8,9,1]
 index_array=[0]*len(array)
 ans=[0]*len(array)
-for index in range (0, len(array)-1): index_array[array[index]]+=1
-for index in range (1, len(index_array)): index_array[index]+=index_array[index-1]
-for index in range (0, len(array)): 
-    ans[index_array[array[index]]]=array[index]
-    index_array[array[index]]-=1
+
+for index in range (0, len(array)-1 ) : 
+    index_array[array[index]] += 1
+
+for index in range (1, len(index_array) ) : 
+    index_array[index] += index_array[index-1]
+
+for index in range (0, len(array) ) : 
+    temp = array[index]
+    ans[ index_array[temp] ] = temp
+    index_array[temp] -= 1
+    
 print(ans)
